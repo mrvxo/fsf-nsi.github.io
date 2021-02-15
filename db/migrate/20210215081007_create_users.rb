e185a5c23612dfc068a1ffb42c69949cfd51975a
mrvxo <mrvxo@protonmail.com>
2021-02-15 09:04:23 +0000

    Changement de Technologie Ruby

---
+++ db/migrate/20210215081007_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.boolean :confirmed, default: false
+      t.string :confirmation_token
+      t.string :password_digest
+      t.boolean :avatar, default:false
+
+      t.timestamps
+    end
+  end
+end
